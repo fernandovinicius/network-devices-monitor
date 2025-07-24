24c266c5f48186e892897038d112d4d4d3729e80
Fernando Vinícius <fernandovinicius1708@gmail.com>
2025-07-24 20:21:28 -0300

    Update connection.py

--- v1/backend/db/connection.py
+++ v1/backend/db/connection.py
@@ -6,7 +6,6 @@ from loguru import logger
 def get_connection():
     try:
         conn = sqlite3.connect(DATABASE_URL, check_same_thread=False)
-        logger.info("Conexão com o banco de dados estabelecida com sucesso.")
         return conn
     except Exception as e:
         logger.error(f"Erro ao estabelecer conexão com o banco de dados: {e}")
