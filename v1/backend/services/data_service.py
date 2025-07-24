e90587bbff7e3c8b587eb0c99b67567b8eb0bc6f
Fernando Vinícius <fernandovinicius1708@gmail.com>
2025-07-24 20:21:44 -0300

    Update data_service.py

--- v1/backend/services/data_service.py
+++ v1/backend/services/data_service.py
@@ -1,4 +1,5 @@
 from db.connection import get_connection
+from models.monitoring_data import MonitoringData
 from loguru import logger
 
 
@@ -9,7 +10,8 @@ def get_monitoring_data(device_id):
         cursor.execute("SELECT * FROM MONITORING_DATA WHERE DEVICE_ID=?", (device_id,))
         rows = cursor.fetchall()
         conn.close()
-        return rows
+        data = [MonitoringData(*row) for row in rows]
+        return data
     except Exception as e:
         logger.error(
             f"Erro ao buscar dados de monitoramento para dispositivo {device_id}: {e}"
