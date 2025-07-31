08e3e03f3dee0d0c669c8e536ddebb8ff2a467cc
Fernando Vinícius <fernandovinicius1708@gmail.com>
2025-07-30 21:00:20 -0300

    Update history_service.py

--- v1/backend/services/history_service.py
+++ v1/backend/services/history_service.py
@@ -3,15 +3,28 @@ from models.device_status_history import DeviceStatusHistory
 from loguru import logger
 
 
-def get_device_history(device_id):
+def get_device_history(device_id, begin_date=None, end_date=None):
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        cursor.execute(
-            "SELECT * FROM DEVICES_STATUS_HISTORY WHERE DEVICE_ID=?", (device_id,)
-        )
+
+        query = "SELECT * FROM DEVICES_STATUS_HISTORY WHERE DEVICE_ID = ?"
+        params = [device_id]
+
+        if begin_date and end_date:
+            query += " AND last_status_change BETWEEN ? AND ?"
+            params.extend([begin_date, end_date])
+        elif begin_date:
+            query += " AND last_status_change >= ?"
+            params.append(begin_date)
+        elif end_date:
+            query += " AND last_status_change <= ?"
+            params.append(end_date)
+
+        cursor.execute(query, tuple(params))
         rows = cursor.fetchall()
         conn.close()
+
         data = [DeviceStatusHistory(*row) for row in rows]
         return data
     except Exception as e:
