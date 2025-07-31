c2904412c8eea0e9b8f14dc39417213dda8fa256
Fernando Vinícius <fernandovinicius1708@gmail.com>
2025-07-30 21:00:37 -0300

    Update device_status.py

--- v1/backend/models/device_status.py
+++ v1/backend/models/device_status.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from flask_restx import fields
 
 
 class DeviceStatus(Enum):
@@ -6,3 +7,20 @@ class DeviceStatus(Enum):
     DOWN = 1
     PAUSED = 2
     NOT_STARTED = 99
+
+
+DEVICE_STATUS_DESC = {
+    "UP": fields.Integer(
+        example=DeviceStatus.UP.value, description="Dispositivo está online"
+    ),
+    "DOWN": fields.Integer(
+        example=DeviceStatus.DOWN.value, description="Dispositivo está offline"
+    ),
+    "PAUSED": fields.Integer(
+        example=DeviceStatus.PAUSED.value, description="Monitoração desabilitada"
+    ),
+    "NOT_STARTED": fields.Integer(
+        example=DeviceStatus.NOT_STARTED.value,
+        description="Monitoramento não iniciado",
+    ),
+}
