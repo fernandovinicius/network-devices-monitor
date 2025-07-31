31ef4ed146c1b41fdcc57fdf025bc3e7bba44bdf
Fernando Vinícius <fernandovinicius1708@gmail.com>
2025-07-30 21:01:34 -0300

    Update history_controller.py

--- v1/backend/controllers/history_controller.py
+++ v1/backend/controllers/history_controller.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+from flask import request
 from flask_restx import Namespace, Resource, fields
 from utils.http_status import (
     HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
     HTTP_500_INTERNAL_ERROR,
 )
+from utils.validation import validate_datetime
 from services.history_service import get_device_history
 from loguru import logger
 
@@ -24,12 +28,40 @@ device_status_history_model = history_ns.model(
 
 
 @history_ns.route("/<int:device_id>")
+@history_ns.param("device_id", "ID do dispositivo")
 class DeviceHistoryResource(Resource):
+    @history_ns.doc(
+        params={
+            "begin_date": {
+                "description": "Data/hora inicial no formato ISO 8601 (ex: 2025-07-29T23:11:32)",
+                "required": False,
+                "type": "string",
+            },
+            "end_date": {
+                "description": "Data/hora final no formato ISO 8601 (ex: 2025-07-30T23:11:32)",
+                "required": False,
+                "type": "string",
+            },
+        }
+    )
     @history_ns.marshal_list_with(device_status_history_model)
     def get(self, device_id):
-        """Retorna o histórico de status de um dispositivo pelo ID"""
+        """Retorna o histórico de status de um dispositivo pelo ID e intervalo de datas (ISO 8601)"""
+
+        # Verifica Datas
+        begin_date = request.args.get("begin_date")
+        end_date = request.args.get("end_date")
+        if (begin_date and not validate_datetime(begin_date)) or (
+            end_date and not validate_datetime(end_date)
+        ):
+            history_ns.abort(
+                HTTP_400_BAD_REQUEST,
+                "Formato de data inválido. Use o formato ISO 8601: YYYY-MM-DDTHH:MM",
+            )
+
+        # Requisição no Banco
         try:
-            history = get_device_history(device_id)
+            history = get_device_history(device_id, begin_date, end_date)
         except Exception as e:
             logger.error(f"Erro ao buscar histórico do dispositivo {device_id}: {e}")
             history_ns.abort(HTTP_500_INTERNAL_ERROR, "Erro interno.")
