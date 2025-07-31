b39e5efdf5f23731d208e4f686126ff79a937fef
Fernando Vinícius <fernandovinicius1708@gmail.com>
2025-07-30 21:00:01 -0300

    Update data_service.py

--- v1/backend/services/data_service.py
+++ v1/backend/services/data_service.py
@@ -3,13 +3,28 @@ from models.monitoring_data import MonitoringData
 from loguru import logger
 
 
-def get_monitoring_data(device_id):
+def get_monitoring_data(device_id, begin_date=None, end_date=None):
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM MONITORING_DATA WHERE DEVICE_ID=?", (device_id,))
+
+        query = "SELECT * FROM MONITORING_DATA WHERE DEVICE_ID = ?"
+        params = [device_id]
+
+        if begin_date and end_date:
+            query += " AND timestamp BETWEEN ? AND ?"
+            params.extend([begin_date, end_date])
+        elif begin_date:
+            query += " AND timestamp >= ?"
+            params.append(begin_date)
+        elif end_date:
+            query += " AND timestamp <= ?"
+            params.append(end_date)
+
+        cursor.execute(query, tuple(params))
         rows = cursor.fetchall()
         conn.close()
+
         data = [MonitoringData(*row) for row in rows]
         return data
     except Exception as e:
